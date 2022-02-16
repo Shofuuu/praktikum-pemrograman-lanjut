113daee7784e26837ee416acfd816945e52cfdd4
shofuuu <hadoepan@gmail.com>
2022-02-16 12:23:36 +0700

    added first program

---
+++ Feb_16_2021.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# example 1
+print('hello world')
+
+# example 2
+print('hello, python!')
+print('Muh. Shofuwan Anwar')
+shofu = 1
+print(shofu)
+
+# example 3
+print('shofuwan')
+print(21)
+data = 'shofuwan'
+print(data)
+
+# example 4
+print("\033[1;31m\'Muh. Shofuwan Anwar\'\033[0m")
