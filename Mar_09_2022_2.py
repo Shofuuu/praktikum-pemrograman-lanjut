e519d422b6ca43d949cd72201f95011652481e72
shofuuu <hadoepan@gmail.com>
2022-03-09 15:23:53 +0700

    Fixed for Mar_09_2

--- Mar_09_2022_2.py
+++ Mar_09_2022_2.py
@@ -12,9 +12,25 @@
 
 kelas = []
 
-for x in range(3):
-    in_mahasiswa, in_mahasiswi = input("Masukkan Jumlah Mahasiswa dan Jumlah Mahasiswi: ").split()
-    combine_kelas = [int(in_mahasiswa), int(in_mahasiswi), int(in_mahasiswa)+int(in_mahasiswi)]
-    kelas.append(combine_kelas)
+print('Opsi :\n\t1. Tambah Data\n\t2. Tampilkan Data [0:mahasiswa, 1:mahasiswi, 2:total]\n\t3. Keluar')
+print('Example : \n\tMemilih tambah data pada kelas ke-1 dengan mahasiswa 15, mahasiswi 18\n\t> 1 15:18')
+print('\tMenampilkan data kelas ke-0 khusus mahasiswi\n\t> 2 0:1')
+print('\tKeluar\n\t> 3 x')
 
-print('Kelas:', kelas, end='\n\n')
+while True:
+    try:
+        opsi, val = input("[opsi][value] > ").split()
+        
+        if int(opsi) == 1:
+            combine_kelas = val.split(':')
+            combine_kelas.append(int(combine_kelas[0])+int(combine_kelas[1]))
+            kelas.append(combine_kelas)
+        elif int(opsi) == 2:
+            tipe_data = ['mahasiswa', 'mahasiswi', 'total']
+            index = val.split(':')
+            print('kelas-', index[0], ', ', tipe_data[int(index[1])], ' : ', kelas[int(index[0])][int(index[1])], sep='')
+        elif int(opsi) == 3:
+            print('--selesai--')
+            break
+    except:
+        print('--Error--', end='\n\n')
